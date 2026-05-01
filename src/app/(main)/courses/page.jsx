2ebabfdfa89ad63f9477f475e6e6bf0c0ca17319
simantopal <simantop13@gmail.com>
2026-05-01 20:14:11 +0600

    create course details page

--- src/app/(main)/courses/page.jsx
+++ src/app/(main)/courses/page.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 const AllCoursePage = async () => {
     const res = await fetch("https://skill-sphere-online-learning-platfo-eight.vercel.app/data.json")
     const courses = await res.json();
-    console.log(courses)
+
     return (
         <div className='container mx-auto'>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-10'>
                 {courses.map(course => (
-                    <TopCourseCard key={courses.id} course={course} />
+                    <TopCourseCard key={course.id} course={course} />
                 ))}
             </div>
         </div>
